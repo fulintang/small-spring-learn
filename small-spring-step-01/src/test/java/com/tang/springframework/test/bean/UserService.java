26619f92ec45dacd9af2d28e9fd16271a7711cbb
fulin.tang <tangtangdeyx@126.com>
2022-07-18 14:33:15 +0800

    init project 1 实现一个简单的Bean容器

---
+++ small-spring-step-01/src/test/java/com/tang/springframework/test/bean/UserService.java
@@ -0,0 +1,16 @@
+package com.tang.springframework.test.bean;
+
+/**
+ * 测试用Bean
+ *
+ * @author tangfulin
+ * @version V3.0
+ * @since 2022/7/18 14:08
+ */
+public class UserService {
+    
+    public void queryUserInfo() {
+        System.out.println("查询用户信息！");
+    }
+    
+}
