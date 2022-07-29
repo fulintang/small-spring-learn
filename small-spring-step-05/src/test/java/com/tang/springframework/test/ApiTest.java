cd63f2bc8f26bd15a75379b94d369704a73a04e1
fulin.tang <tangtangdeyx@126.com>
2022-07-29 11:18:01 +0800

    编写文件加载测试类

--- small-spring-step-05/src/test/java/com/tang/springframework/test/ApiTest.java
+++ small-spring-step-05/src/test/java/com/tang/springframework/test/ApiTest.java
@@ -44,7 +44,7 @@ public class ApiTest {
 
     @Test
     public void testUrl() throws IOException {
-        Resource resource = resourceLoader.getResource("https://github.com/fuzhengwei/small-spring/important.properties");
+        Resource resource = resourceLoader.getResource("https://github.com/fulintang/small-spring-learn/blob/main/small-spring-step-05/src/main/resources/important.properties");
         InputStream inputStream = resource.getInputStream();
         String content = IoUtil.readUtf8(inputStream);
         System.out.println(content);
